c67b0fe9f2956c5ed26ff9a3d116ef7914002080
Eduard Belchik <stor16@gmail.com>
2021-03-11 22:24:03 +0200

    fix logic from sObject to inputs

--- aura forms/object creation custom form/createAnimalController.js
+++ aura forms/object creation custom form/createAnimalController.js
@@ -1,14 +1,21 @@
 ({
 	save : function(component, event, helper) {
 
-        var name = component.get('v.createAnimal.Name');
+        var name = component.get('v.name');
+        var eats = component.get('v.eats');
+        var says = component.get('v.says');
+        var extId = component.get('v.extId');
         if(name == undefined || name == null || name == '') {
          	helper.showToast('Ooops !', 'Please fill up name', 'error');
         }
         else {
         	var action = component.get('c.saveAnimal');
         	action.setParams({
-            	animal : component.get('v.createAnimal')
+            	name : name,
+                eats : eats,
+                says : says,
+                extId : extId
+                
         	});
         	action.setCallback(this,function(response){
                 var state = response.getState();
