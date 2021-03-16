9f2a588dc9b17c2e768e7a65f0a61e21a020a850
Eduard Belchik <stor16@gmail.com>
2021-03-17 01:56:40 +0200

    home work 8 : create animals; send email

--- createAnimalsFromWebService.js
+++ createAnimalsFromWebService.js
@@ -1,6 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import getAnimalInfoListByIdRange from '@salesforce/apex/Animal.getAnimalInfoListByIdRange';
 import createAnimals from '@salesforce/apex/Animal.createAnimals';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 const columns = [
     { label: 'ID', fieldName: 'ExternalID__c' },
@@ -9,32 +10,108 @@ const columns = [
     { label: 'Eats', fieldName: 'Eats__c' }
 ];
 
+function showToast (title, message, variant) {
+    const evt = new ShowToastEvent({
+        title: title,
+        message: message,
+        variant: variant,
+        mode: 'dismissable'
+    });
+    dispatchEvent(evt);
+}
+
 export default class CreateAnimalsFromWebService extends LightningElement {
 
     minExtId = 1;
     maxExtId = 5;
     columns = columns;
     animalList = [];
+    @track isExtIdDisabled = false;
+    @track isBtnGetAnimalDisabled = false;
+    @track isBtnCreateAnimalsDisabled = true;
+
+    handleMinExtId(event) {
+        this.minExtId = event.detail.value;
+        let errorMessage = '';
+
+        if(this.maxExtId < this.minExtId || this.minExtId > this.maxExtId) {
+            errorMessage = errorMessage + '\n\'External ID max\' can\'t be less than \'External ID min\'';
+        }
+    
+        if(this.minExtId == '' || this.minExtId == null || this.minExtId == undefined) {
+            errorMessage = errorMessage + '\nYou should fill \'External ID min\' field';
+        }
+    
+        if(this.maxExtId == '' || this.maxExtId == null || this.maxExtId == undefined) {
+            errorMessage = errorMessage + '\nYou should fill \'External ID max\' field';
+        }
+
+        if(errorMessage == '') {
+            this.isBtnGetAnimalDisabled = false;
+        } else {
+            this.isBtnGetAnimalDisabled = true;
+            showToast('Error !', errorMessage, 'error');
+        }
+    }
+
+    handleMaxExtId(event) {
+        this.maxExtId = event.detail.value;
+        let errorMessage = '';
+
+        if(this.maxExtId < this.minExtId || this.minExtId > this.maxExtId) {
+            errorMessage = errorMessage + '\n\'External ID max\' can\'t be less than \'External ID min\'';
+        }
+    
+        if(this.minExtId == '' || this.minExtId == null || this.minExtId == undefined) {
+            errorMessage = errorMessage + '\nYou should fill \'External ID min\' field';
+        }
+    
+        if(this.maxExtId == '' || this.maxExtId == null || this.maxExtId == undefined) {
+            errorMessage = errorMessage + '\nYou should fill \'External ID max\' field';
+        }
+
+        if(errorMessage == '') {
+            this.isBtnGetAnimalDisabled = false;
+        } else {
+            this.isBtnGetAnimalDisabled = true;
+            showToast('Error !', errorMessage, 'error');
+        }
+    }
 
     getAnimalBtnClickHandler() {
+
+        this.isBtnGetAnimalDisabled = true;
+        this.isExtIdDisabled = true;
+
         getAnimalInfoListByIdRange({ minExtId : this.minExtId,
                                     maxExtId : this.maxExtId
         }).then(result => {
             if(result) {
-                console.log(result);
                 this.animalList = result;
+                this.isBtnCreateAnimalsDisabled = false;
+                showToast('Success !', 'Animals Retrieved Successfully', 'success');
             }
             else if(error) {
+                this.isBtnCreateAnimalsDisabled = true;
                 let errorMessage = "Error: " + error;
-                alert(errorMessage);
-                console.log(errorMessage);
+                showToast('Error !', errorMessage, 'error');
             }
-        });   
+        });
+
+        this.isBtnGetAnimalDisabled = false;
+        this.isExtIdDisabled = false;
     }
 
     createAnimalsBtnClickHandler() {
-        console.log(this.template.querySelector('lightning-datatable').getSelectedRows());
-        createAnimals({arrayOfAnimals : this.template.querySelector('lightning-datatable').getSelectedRows()})
+        let selectedRows = this.template.querySelector('lightning-datatable').getSelectedRows();
+        if (selectedRows.length == 0) {
+            showToast('Warning', 'Select one or more rows to create animals', 'warning');
+            return;
+        }
+        console.log(selectedRows.length);
+        createAnimals({
+            animals : selectedRows
+        }).then(showToast('Success !', 'Animals Created Successfully', 'success'));
     }
 
 }
